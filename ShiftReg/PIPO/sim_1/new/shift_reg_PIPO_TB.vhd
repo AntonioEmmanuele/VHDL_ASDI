90f9f7fc9b253d499c4059b1e180ff1eaf0e9729
Antonio Emmanuele <58343836+AntonioEmmanuele@users.noreply.github.com>
2022-02-02 17:56:39 +0100

    Fixato e testato comportamentale

--- ShiftReg/PIPO/sim_1/new/shift_reg_PIPO_TB.vhd
+++ ShiftReg/PIPO/sim_1/new/shift_reg_PIPO_TB.vhd
@@ -1,43 +1,102 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02.02.2022 14:28:29
--- Design Name: 
--- Module Name: shift_reg_PIPO_TB - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity shift_reg_pipo_tb is
+end;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture bench of shift_reg_pipo_tb is
 
-entity shift_reg_PIPO_TB is
---  Port ( );
-end shift_reg_PIPO_TB;
+  component shift_reg_pipo
+      generic(
+          N:positive:=4;
+          M:positive:=4
+          );
+        port(
+          RST: IN std_logic;
+          CLK: IN std_logic;
+          MEM_ENABLE: IN std_logic;
+          SHIFT_ENABLE: IN std_logic;
+          RIGHT_SHIFT: IN std_logic;
+          Y: IN std_logic_vector(0 to M-1);
+          X: IN std_logic_vector(0 to N-1);
+          PARALLEL_OUT: OUT std_logic_vector(0 to N-1)
+        );
+  end component;
 
-architecture Behavioral of shift_reg_PIPO_TB is
+  signal RST: std_logic:='1';
+  signal CLK: std_logic;
+  signal MEM_ENABLE: std_logic:='0';
+  signal SHIFT_ENABLE: std_logic:='0';
+  signal RIGHT_SHIFT: std_logic:='0';
+  signal Y: std_logic_vector(0 to 3);
+  signal X: std_logic_vector(0 to 3);
+  signal PARALLEL_OUT: std_logic_vector(0 to 3) ;
+
+  constant clock_period: time := 50 ns;
+    
 
 begin
 
+  
+  uut: shift_reg_pipo generic map ( N            =>4 ,
+                                    M            =>4  )
+                         port map ( RST          => RST,
+                                    CLK          => CLK,
+                                    MEM_ENABLE   => MEM_ENABLE,
+                                    SHIFT_ENABLE => SHIFT_ENABLE,
+                                    RIGHT_SHIFT  => RIGHT_SHIFT,
+                                    Y            => Y,
+                                    X            => X,
+                                    PARALLEL_OUT => PARALLEL_OUT );
+   -- Genero un clock periodico.
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for clock_period/2;
+		CLK <= '1';
+		wait for clock_period/2;
+   end process;
+ 
+  --
+  stimulus: process
+-------------------
+  begin   
+    
+       
+    wait for  clock_period+clock_period/2 ; -- a 25 ns out=0 dopo a 75 il dato diventa F.
+        assert PARALLEL_OUT="0000" -- Per i primi 25 ns sara' undefined poi assume il valore di 0
+        report " Errore nel reset"
+        severity failure;
+        RST<='0'; -- Tolgo il reset
+        MEM_ENABLE<='1'; -- Abilito la memorizzazione.
+        SHIFT_ENABLE<='0';
+        X<="1111";
+    wait for clock_period ; 
+        assert PARALLEL_OUT="1111" 
+        report " Errore nel prendere il valore"
+        severity failure;
+        MEM_ENABLE<='0'; -- Disattivo la memorizzazione
+        RIGHT_SHIFT<='1';--Shifto a destra
+        Y<=std_logic_vector(to_unsigned(2,4));-- Shifto di un bit
+        SHIFT_ENABLE<='1';-- Voglio shiftare 
+    wait for clock_period ; 
+        assert PARALLEL_OUT="0011" 
+        report " Errore nello shift"
+        severity failure;
+        --Andiamo a shiftare di 2 posizioni
+        SHIFT_ENABLE<='0';  -- Lo abbasso per evitare conflitti
+        RIGHT_SHIFT<='0';   -- LO voglio a sinistra 
+        Y<=std_logic_vector(to_unsigned(3,4));-- 3 posizioni, in modo che io abbia 1000
+        SHIFT_ENABLE<='1';  -- Lo riabilito
+    wait for clock_period;
+        assert PARALLEL_OUT="1000" 
+        report " Errore nello shift"
+        severity failure;
 
-end Behavioral;
+  end process;
+
+
+
+end;
+  
--- ShiftReg/PIPO/sources_1/new/shift_reg_pipo.vhd
+++ ShiftReg/PIPO/sources_1/new/shift_reg_pipo.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------
--- Company: 
+-- Company:  Esame Architettura Dei 
 -- Engineer: 
 -- 
 -- Create Date: 02.02.2022 11:58:19
@@ -43,61 +43,58 @@ entity shift_reg_pipo is
         MEM_ENABLE: IN std_logic;                           --      Questo segnale abilita la memorizzazione . quindi quando questo segnale e' alto viene memorizzato il dato.
         SHIFT_ENABLE: IN std_logic;                         --      Questo segnale abilita la modalita' di shift, quindi quando questo segnale e' alto lo SR va a shiftare di y bits.
         RIGHT_SHIFT: IN std_logic;                          --      Questo segnale vale 1 se vogliamo shiftare a destra >>, se vale 0 allora dobbiamo shiftare a sinistra.
-        Y: IN std_logic_vector(0 to M);                     --      Questo segnale fornisce il numero di bits da shiftare scelto dato in input dall'utente.
+        Y: IN std_logic_vector(0 to M-1);                   --      Questo segnale fornisce il numero di bits da shiftare scelto dato in input dall'utente.
         X: IN std_logic_vector(0 to N-1);                   --      Questo e' l'input fornito dall'utente.
         PARALLEL_OUT: OUT std_logic_vector(0 to N-1)        --      Questo e' l'output fornito dal registro. 
       );
 end shift_reg_pipo;
 
 architecture Behavioral of shift_reg_pipo is
-     --  Ci dichiariamo una segnale di appoggio  su cui andare ad operare (memorizzare e shiftare)
-     --  Si noti che il caricamento parallelo viene fatto mettendo reg(0):=x(0) reg(1):=x(1) ecc
-     signal h: std_logic_vector(N-1 downto 0) := std_logic_vector(to_unsigned(0,N)); 
+        --  Ci dichiariamo una segnale di appoggio  su cui andare ad operare (memorizzare e shiftare)
+        --  Si noti che il caricamento parallelo viene fatto mettendo reg(0):=x(0) reg(1):=x(1) ecc
+       -- signal h: std_logic_vector(N-1 downto 0) := std_logic_vector(to_unsigned(0,N)); 
 begin
-   s_r:process(CLK)
+    s_r:process(CLK)
+        variable h: std_logic_vector(0 to N-1) := std_logic_vector(to_unsigned(0,N));
         begin
-            if(rising_edge(clk)) then 
-                if(RST='1') then -- Se dobbiamo resettare allora 
-                    h<=std_logic_vector(to_unsigned(0,N)); -- resetta il valore iniziale
-                else -- altrimenti, se non devo resettare.
-                    if(MEM_ENABLE='1') then  -- Se devo memorizzare 
-                        h<=X; 
-                    elsif( MEM_ENABLE='0' and SHIFT_ENABLE='1' ) then --  POSSO SHIFTARE SOLO QUANDO NON DEVO MEMORIZZARE E GLI FORNISCO IL COMANDO DI SHIFT.
-                        -- Ora posso shiftare sia a sinistra, sia a destra, i due casi in ogni caso sono tra loro simmetrici.
-                        -- Shift a destra.
-                       if(RIGHT_SHIFT='1') then
-                            -- Assegna, da  verso destra 3 bits ad 1.
-                            -- Ad esempio si supponga di volere: x(0)  x(1) x(2) x(3) allora i primi tre sarebbero 1 e 'ultimo sarebbe x(0)
-                            -- Quindi dovrei come prima cosa mettere x(3) =x(0) e poi i primi tre ad 1
-                            -- 1 1 1 X(0)
-                            -- Analogamente se shiftassi di 2 avrei: 1 1 X(0) X(1)
-                            
-                            -- Assegna i bits da Y ad N-1
-                            h(to_integer(unsigned(Y)) to N-1) <=h(0 to to_integer(unsigned(Y))-1);
-                            -- Metti 1 nei bits iniziali 
-                            h(0 to (to_integer(unsigned(Y))-1))<= std_logic_vector(to_unsigned(1,to_integer(unsigned(Y)))) ;       
-                            --Shift a sinistra e' il complementare.
-                       else       
-                            -- Supponiamo ad esempio di voler shiftare a sinistra di due posizioni.
-                            -- X(0) X(1) X(2) X(3) 
-                            -- X(2) X(3) 1    1     2 bits shift.
-                            
-                            -- Ai primi y bits vanno da Y ad N-1 (2,3 nel nostro esempio)                    
-                            h(0 to to_integer(unsigned(Y))-1)<=h(to_integer(unsigned(Y)) to N-1);
-                            -- Ai valori che vanno da Y ad N-1 va messo 1.
-                            h(to_integer(unsigned(Y)) to N-1)<= std_logic_vector(to_unsigned(1,to_integer(unsigned(Y)))) ;    
-                                           
-                       end if;-- controllo right shift
-                    end if; -- controllo mem enable
-                end if; -- controllo rst          
-            end if; --controllo ck
-   end process s_r;
+        if(rising_edge(CLK)) then 
+            if(RST='1') then -- Se dobbiamo resettare allora 
+                h:=std_logic_vector(to_unsigned(0,N)); -- resetta il valore iniziale
+            else -- altrimenti, se non devo resettare.
+                if(MEM_ENABLE='1') then  -- Se devo memorizzare 
+                    h:=X; 
+                elsif( MEM_ENABLE='0' and SHIFT_ENABLE='1' ) then --  POSSO SHIFTARE SOLO QUANDO NON DEVO MEMORIZZARE E GLI FORNISCO IL COMANDO DI SHIFT.
+                    -- Ora posso shiftare sia a sinistra, sia a destra, i due casi in ogni caso sono tra loro simmetrici.
+                    -- Shift a destra.
+                if(RIGHT_SHIFT='1') then
+                     -- Ad esempio si supponga di volere: x(0)  x(1) x(2) x(3) allora i primi tre sarebbero 0 e 'ultimo sarebbe x(0)
+                     -- Quindi dovrei come prima cosa mettere x(3) =x(0) e poi i primi tre ad 1
+                     -- 0 0 0 X(0)
+                     -- Analogamente se shiftassi di 2 avrei: 0 0 X(0) X(1)     
+                                 
+                    -- Nell'esempio precedente X(3)=X(1) ed X(0)=X(1) la formula e' X(i)=X(i-Y) per i [Y, N-1]                
+                    for i in to_integer(unsigned(Y)) to N-1 loop 
+                        h(i):=h(i-to_integer(unsigned(Y)));
+                    end loop;
+                    -- X(0),X(1) =0 , X(i)=0 per i [0,Y-1]
+                    for i in 0 to to_integer(unsigned(Y))-1  loop 
+                        h(i):='0';
+                    end loop;   
+                --Shift a sinistra e' il complementare.
+                else       
+                      -- X(i)=X(Y+i) per i compreso tra 0 ed N-Y-1, ad esempio se si vuole shiftare di 2 1111 allora X(0)=X(2) ed X(1)=X(3)
+                      for i in N-1-to_integer(unsigned(Y)) downto 0  loop 
+                        h(i):=h(to_integer(unsigned(Y))+i);
+                      end loop;                               
+                      -- X(i) =0 per i [N-Y,N] , nell'esempio precedente questo si traduce come 1100
+                      for i in  N-to_integer(unsigned(Y)) to N-1 loop 
+                        h(i):='0';
+                      end loop;                
+                end if;-- controllo right shift
+            end if; -- controllo mem enable   
+        end if; -- controllo rst    
+        PARALLEL_OUT<=h;      
+     end if; --controllo ck
+    end process s_r;
     
-    --Questo process copia lo stato h in uscita. 
-    assign: process(CLK)
-        begin
-            if(rising_edge(CLK)) then
-                PARALLEL_OUT<=h;
-            end if;    
-    end process assign;
 end Behavioral;
