b562d1720a8f0031f10c43ad719ea68d36a510a2
Antonio Emmanuele <58343836+AntonioEmmanuele@users.noreply.github.com>
2022-02-05 16:27:49 +0100

    Update sift_reg_pipo_TB.vhd

--- ShiftReg/PIPO_Structural/sim_1/new/sift_reg_pipo_TB.vhd
+++ ShiftReg/PIPO_Structural/sim_1/new/sift_reg_pipo_TB.vhd
@@ -7,46 +7,40 @@ end;
 
 architecture bench of shift_reg_pipo_tb is
 
-  component shift_reg_pipo
-      generic(
-          N_bits:integer:=4;
-          M_shift:integer:=2
-          );
-        port(
-          RST: IN std_logic;
-          CLK: IN std_logic;
-          RIGHT_SHIFT: IN std_logic;
-          Y: IN std_logic_vector(0 to M_shift-1);
-          X: IN std_logic_vector(0 to N_bits-1);
-          PARALLEL_OUT: OUT std_logic_vector(0 to N_bits-1);
-          MUX_VALUES: OUT std_logic_vector(0 to N_bits-1)
-        );
+  component shift_reg_pipo 
+  generic (
+    N_Bits:integer:=4;
+    Shift_Bits:integer:=2
+  );
+  port(
+      CLK: in std_logic;
+      RST: in std_logic;
+      shift: in std_logic_vector(0 to Shift_Bits-1);
+      right_shift:in std_logic;
+      Par_in: in std_logic_vector(0 to N_Bits-1);
+      Par_out:out std_logic_vector(0 to N_Bits-1)
+      );
   end component;
-  constant M_shift:integer:=2;
-  constant N_bits:integer:=4;
-  signal RST: std_logic:='1';
-  signal CLK: std_logic;
-  signal RIGHT_SHIFT: std_logic:='0';
-  signal Y: std_logic_vector(0 to M_shift-1);
-  signal X: std_logic_vector(0 to N_bits-1);
-  signal PARALLEL_OUT: std_logic_vector(0 to N_bits-1) ;
-  signal MUX_VALUES: std_logic_vector(0 to N_bits-1) ;
-  constant clock_period: time := 50 ns;
-    
 
+  signal CLK: std_logic;
+  signal right_shift: std_logic:='0';
+  signal RST: std_logic:='1';
+  signal shift: std_logic_vector(0 to 1);
+  signal Par_in: std_logic_vector(0 to 3):="1011";
+  signal Par_out: std_logic_vector(0 to 3) ;
+  constant clock_period : time:=25 ns;
+  
 begin
 
-  
-  uut: shift_reg_pipo generic map ( N_bits            =>N_bits ,
-                                    M_shift           =>M_shift  )
-                         port map ( RST          => RST,
-                                    CLK          => CLK,
-                                    RIGHT_SHIFT  => RIGHT_SHIFT,
-                                    Y            => Y,
-                                    X            => X,
-                                    PARALLEL_OUT => PARALLEL_OUT,
-                                    MUX_VALUES=>MUX_VALUES );
-   -- Genero un clock periodico.
+  uut: shift_reg_pipo generic map(N_Bits=>4,Shift_Bits=>2)
+                    port map ( CLK     => CLK,
+                               RST     => RST,
+                               shift   => shift,
+                               right_shift=> right_shift,
+                               Par_in  => Par_in,
+                               Par_out => Par_out );
+    
+       -- Genero un clock periodico.
    CLK_process :process
    begin
 		CLK <= '0';
@@ -54,48 +48,161 @@ begin
 		CLK <= '1';
 		wait for clock_period/2;
    end process;
- 
- 
   stimulus: process
-    variable not_rev: std_logic_vector(0 to 3);
-    variable rev: std_logic_vector(3 downto 0);
-    begin
-
-    wait for  clock_period+clock_period/2 ; -- a 25 ns out=0 dopo a 75 il dato diventa F.
-            assert PARALLEL_OUT="0000" -- Per i primi 25 ns sara' undefined poi assume il valore di 0
+  begin
+      wait for  clock_period+clock_period/2 ; -- a 25 ns out=0 dopo a 75 il dato diventa F.
+            assert Par_out="0000" -- Per i primi 25 ns sara' undefined poi assume il valore di 0
             report " Errore nel reset"
             severity failure;
             RST<='0';   -- Tolgo il reset  
-            Y<="00";    -- "00" significa modalita' di memorizzazione    
-            X<="1101";
-            
-    -- TEST 1 BIT SHIFT  DESTRA.
-    wait for clock_period+2ns ; 
-        assert PARALLEL_OUT="1101" 
+            shift<="00";   
+--  TEST A SINISTRA          
+--  TEST 1 BIT SHIFT  .
+    wait for clock_period ; 
+        assert Par_out="1011" 
         report " Errore nel prendere il valore"
         severity failure;
-        RIGHT_SHIFT<='1';   --Destra
-        Y<="01";
-    
-    -- TEST 2 BIT SHIFT  DESTRA.
-    wait for clock_period+100ns ; 
-        assert PARALLEL_OUT="0001" 
-        report " Errore nello shiftare a destra il valore"
+        shift<="01";
+    wait for clock_period+clock_period/2 ; 
+        assert Par_out="0110" 
+        report " Errore nello shiftare  il valore di 1 a sinistra"
         severity failure;
-        RIGHT_SHIFT<='1';   --Destra
-        Y<="01";
-    wait;
-    rev:="1011";
-    not_rev(3):=rev(3);
-    not_rev(2):=rev(2); 
-    not_rev(1):=rev(1);
-    not_rev(0):=rev(0);    
-    assert not_rev="1101"
-    severity failure;
-    wait;
+    wait for clock_period ; 
+        assert Par_out="1100" 
+        report " Errore nello shiftare  il valore di 1 a sinistra"
+        severity failure;
+     wait for clock_period ; 
+        assert Par_out="1000" 
+        report " Errore nello shiftare  il valore di 1 a sinistra"
+        severity warning;
+     wait for clock_period ; 
+        assert Par_out="0000" 
+        report " Errore nello shiftare  il valore di 1 a sinstra"
+        severity warning;     
+        shift<="00";
+        Par_in<="1111";
+        
+     -- Test shift di 2 bits
+     wait for clock_period +clock_period/2; 
+        assert Par_out="1111" 
+        report  " Errore nel prendere il valore"
+        severity failure;     
+        shift<="10";
+    wait for clock_period+clock_period/2;
+        assert Par_out="1100" 
+        report  " Errore nello shiftare  il valore di 2 a sinistra"
+        severity failure;     
+    wait for clock_period;
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 2 a sinistra"
+        severity failure;   
+        Par_in<="1101";  
+        shift<="00"  ;  
+    wait for clock_period +clock_period/2; 
+        assert Par_out="1101" 
+        report  " Errore nel prendere il valore"
+        severity failure;       
+        shift<="10";
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="0100" 
+        report  " Errore nello shiftare  il valore di 2 a sinistra"
+        severity failure;     
+     wait for clock_period ; 
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 2 a sinistra"
+        severity failure;     
+        shift<="00";
+        Par_in<="1111";
+   -- Test shift 3 bits.
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="1111" 
+        report  " Errore nel prendere il valore"
+        severity failure;   
+        shift<="11";                 
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="1000" 
+        report  " Errore nello shiftare  il valore di 3 a sinitra"
+        severity failure;          
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 3 a sinitra"
+        severity failure;
+        right_shift<='1';
+        Par_in<="1101";
+        shift<="00";            
+    -- TEST A DESTRA        
+    -- TEST 1 BIT SHIFT  
+    wait for clock_period ; 
+        assert Par_out="1101" 
+        report " Errore nel prendere il valore"
+        severity warning;
+        shift<="01";
+    wait for clock_period ; 
+        assert Par_out="0101" 
+        report " Errore nello shiftare  il valore di 1 a destra"
+        severity warning;
+    wait for clock_period ; 
+        assert Par_out="0010" 
+        report " Errore nello shiftare  il valore di 1 a destra"
+        severity warning;
+     wait for clock_period ; 
+        assert Par_out="0001" 
+        report " Errore nello shiftare  il valore di 1 a destra"
+        severity warning;
+     wait for clock_period ; 
+        assert Par_out="0000" 
+        report " Errore nello shiftare  il valore di 1 a destra"
+        severity warning;     
+        shift<="00";
+        Par_in<="1111";
+        
+     -- Test shift di 2 bits
+     wait for clock_period +clock_period/2; 
+        assert Par_out="1111" 
+        report  " Errore nel prendere il valore"
+        severity failure;     
+        shift<="10";
+    wait for clock_period;
+        assert Par_out="0011" 
+        report  " Errore nello shiftare  il valore di 2 a destra"
+        severity failure;     
+    wait for clock_period;
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 2 a destra"
+        severity failure;   
+        Par_in<="1011";  
+        shift<="00"  ;  
+    wait for clock_period +clock_period/2; 
+        assert Par_out="1011" 
+        report  " Errore nel prendere il valore"
+        severity failure;       
+        shift<="10";
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="0010" 
+        report  " Errore nello shiftare  il valore di 2 a destra"
+        severity failure;     
+     wait for clock_period ; 
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 2 a destra"
+        severity failure;     
+        shift<="00";
+        Par_in<="1111";
+   -- Test shift 3 bits.
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="1111" 
+        report  " Errore nel prendere il valore"
+        severity failure;   
+        shift<="11";                 
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="0001" 
+        report  " Errore nello shiftare  il valore di 3 a destra"
+        severity failure;          
+     wait for clock_period+clock_period/2 ; 
+        assert Par_out="0000" 
+        report  " Errore nello shiftare  il valore di 3 a destra"
+        severity failure;
+   wait;
   end process;
 
 
-
 end;
-  
