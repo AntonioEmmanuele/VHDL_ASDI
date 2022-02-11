9ff80bbe9847dbec3bf35554010a218f867464b3
Antonio Emmanuele <58343836+AntonioEmmanuele@users.noreply.github.com>
2022-02-11 20:00:18 +0100

    Update mem.vhd

--- Cronometro/sources_1/new/mem.vhd
+++ Cronometro/sources_1/new/mem.vhd
@@ -2,26 +2,26 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-entity ROM is
+entity Memory is
 generic(
     N: integer :=4 ;
     M: integer := 17;
-    CK_Period:time :=10ns
+    N_BitNum: integer:=2
 );
 port(
         input:in std_logic_vector( 0 to M-1);
         enable:in std_logic;
         rst: in std_logic;
         ck: in std_logic;
-        mem:in std_logic_vector( 0 to N-1 );
-        sel: in std_logic_vector(0 to N-1);
+        mem:in std_logic_vector( 0 to N_BitNum-1 );
+        sel: in std_logic_vector(0 to N_BitNum-1);
         output: out std_logic_vector(0 to M-1)
     );
-end ROM;
+end Memory;
 
-architecture Behavioral of ROM is
+architecture Behavioral of Memory is
     TYPE matrix IS ARRAY ( 0 to N-1) OF std_logic_vector(0 to M-1); -- Non l'ho dichiarato come una matrice ma come un array di std_logic _vector, cosi' posso fare assegnazione di intera riga
-    signal rom:matrix:=(others=>(others=>'0'));
+    signal mat:matrix:=(others=>(others=>'0'));
     --signal mem_intermediate: std_logic_vector( 0 to N-1):=(others=>'0');
     --signal sel_intermediate:  std_logic_vector(0 to N-1):=(others=>'0');
 begin
@@ -30,12 +30,12 @@ begin
     begin
         if(rising_edge(ck)) then
             if(rst='1') then
-                rom<=(others=>(others=>'0'));
+                mat<=(others=>(others=>'0'));
             end if;
             if(enable='1') then 
-                rom(to_integer(unsigned(mem)) )<=input ;
+                mat(to_integer(unsigned(mem)) )<=input ;
             end if;
-            output<=rom(to_integer(unsigned(sel))) ;
+            output<=mat(to_integer(unsigned(sel))) ;
          end if;
     end process;
 
