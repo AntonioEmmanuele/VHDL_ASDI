7481479cdcf79195960ce5122cfb0b14c6bd6ac0
Antonio Emmanuele <58343836+AntonioEmmanuele@users.noreply.github.com>
2022-03-18 12:34:14 +0100

    Update Counter_mod_N.vhd

--- Handshake/EsercizioHandshake/sources_1/new/Counter_mod_N.vhd
+++ Handshake/EsercizioHandshake/sources_1/new/Counter_mod_N.vhd
@@ -5,7 +5,7 @@ use IEEE.Numeric_Std.all;
 entity counter_mod_n is
     generic(
         N           : integer :=16;                                -- Max value
-        Bit_number  : integer := 4;                                -- 2^Bit_number al più = N
+        Bit_number  : integer := 4;                                -- 2^Bit_number al piÃ¹ = N
         CLK_period  : time := 1000ms                               -- Periodo clock, supposto 1s
     );
     port(
@@ -20,7 +20,7 @@ entity counter_mod_n is
 end counter_mod_n;
 
 architecture Behavioral of counter_mod_n is
-signal count : std_logic_vector (0 to Bit_number-1):=(others=>'0');
+signal count : std_logic_vector (0 to Bit_number-1);
     begin
     
     update: process(ck)
@@ -28,28 +28,21 @@ signal count : std_logic_vector (0 to Bit_number-1):=(others=>'0');
         if (ck'event and ck='0') then
             if (rst = '1') then 
                 count <= (others=>'0');
+                cnt_done<='0';    
             elsif (load = '1') then
                 count <= input_value;
+                cnt_done<='0';    
             elsif (enable = '1') then
                 if(to_integer(unsigned(count)) >= N-1) then 
-                    count <= (others=>'0');                    
+                    count <= (others=>'0');
+                    cnt_done<='1';                    
                 else
-                    count <= std_logic_vector(unsigned(count)+1);                   
+                    count <= std_logic_vector(unsigned(count)+1);
+                    cnt_done<='0';                 
                 end if;-- if del load
             end if; -- if del reset
         end if;-- if del clock
     end process update;
-    
-    strobe: process (count)
-        begin
-            if(to_integer(unsigned(count)) = N-1) then
-                cnt_done<='1';
-            else
-                cnt_done<='0';
-            end if;
-    end process strobe;
-    
-    
     
     count_value <= count;
 
